8cba7d3d1a687cb1c31a103ec5dcb049cb188468
Beeko <bcobine@gmail.com>
2015-09-07 09:52:20 -0700

    Various changes

--- full_fireball.mk
+++ full_fireball.mk
@@ -12,30 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
 # Inherit device configuration
 $(call inherit-product, device/htc/fireball/device_fireball.mk)
 
 # Device naming
 PRODUCT_DEVICE := fireball
-PRODUCT_NAME := pac_fireball
+PRODUCT_NAME := full_fireball
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := Incredible 4G LTE
 PRODUCT_MANUFACTURER := HTC
-
-# Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=fireball \
-    BUILD_ID=IMM76D \
-    BUILD_FINGERPRINT="verizon_wwe/fireball/fireball:4.0.4/IMM76D/278117.2:user/release-keys" \
-    PRIVATE_BUILD_DESC="2.19.605.2 CL278117 release-keys"
-
-# Access VZW apps in Play store
-PRODUCT_GMS_CLIENTID_BASE := android-verizon
 
 # Release name
 PRODUCT_RELEASE_NAME := fireball
