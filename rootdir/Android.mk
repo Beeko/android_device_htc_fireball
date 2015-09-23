bb452f18de58cb9799ffa141a1aff551adcb2fe6
Beeko <bcobine@gmail.com>
2015-09-23 15:27:10 -0700

    LED at boot Hopefully I finally got this thing figured out.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -29,3 +29,11 @@ LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/init.target.rc
 LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       	:= init.led.sh
+LOCAL_MODULE_TAGS  	:= optional eng
+LOCAL_MODULE_CLASS 	:= ETC
+LOCAL_SRC_FILES    	:= etc/init.led.sh
+LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
