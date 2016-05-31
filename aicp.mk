42ce3a0e70f12accc8abb25815d3cb952ce2c9a5
Brian C <bcobine@gmail.com>
2016-05-31 08:35:42 -0700

    Welcome AICP

--- aicp.mk
+++ aicp.mk
@@ -15,13 +15,13 @@
 $(call inherit-product, device/htc/fireball/full_fireball.mk)
 
 # Inherit CM full phone configuration
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/configs/common.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aicp/configs/nfc_enhanced.mk)
 
 # Device naming
-PRODUCT_NAME := cm_fireball
+PRODUCT_NAME := aicp_fireball
 
 # Override build props
 PRODUCT_BUILD_PROP_OVERRIDES += \
